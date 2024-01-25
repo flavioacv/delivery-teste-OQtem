9e91198fac2b5047abc6b397b03ed13bc6c2840c
Flavio Augusto <flavioacv.soft@gmail.com>
2024-01-25 13:40:34 -0400

    config flutter plugin web

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:teste_delivery/app/app_module.dart';
 import 'package:teste_delivery/app/app_widget.dart';
 
 void main() async {
+  usePathUrlStrategy();
   runApp(
     ModularApp(
       module: AppModule(),
