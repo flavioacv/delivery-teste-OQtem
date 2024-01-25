1b7c4a08e298c0d259f02db024894310efd52020
Flavio Augusto <flavioacv.soft@gmail.com>
2024-01-25 10:35:09 -0400

    configuração main e app_modular

---
+++ lib/app/app_module.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppModule extends Module {
+  @override
+  void routes(RouteManager r) {
+    // r.module(
+    //   Modular.initialRoute,
+    //   module: SignInModule(),
+    // );
+  }
+}
