9854d1ab2cdd746f558b2d4b0e71422a68e635ee
Flavio Augusto <flavioacv.soft@gmail.com>
2024-01-25 15:31:12 -0400

    config para rodar web e desktop

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+//import 'package:flutter_web_plugins/flutter_web_plugins.dart'; // se for rodar web , descomentar essa linha
 import 'package:teste_delivery/app/app_module.dart';
 import 'package:teste_delivery/app/app_widget.dart';
 
 void main() async {
-  usePathUrlStrategy();
+  // usePathUrlStrategy(); // se for rodar web , descomentar essa linha
+
   runApp(
     ModularApp(
       module: AppModule(),
