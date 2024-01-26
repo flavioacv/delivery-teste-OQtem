4c774ef086798bb06115e56056bd21aa9533fecc
Flavio Augusto <flavioacv.soft@gmail.com>
2024-01-26 17:49:27 -0400

    formatacao de data

--- lib/app/core/service/formatters/date_extension.dart
+++ lib/app/core/service/formatters/date_extension.dart
@@ -1,10 +1,11 @@
-//import 'package:intl/intl.dart';
+import 'package:intl/intl.dart';
 
-// extension DateExtension on DateTime {
-//   String get toDateDDMMYYString => DateFormat('dd/MM/yyyy').format(this);
-//   String get toTimeHM => DateFormat('HH:mm').format(this);
-//   String get formatToUsaString => DateFormat('yyyy-MM-dd HH:mm:ss').format(this);
-// }
+extension DateExtension on DateTime {
+  String get toDateDDMMYYString => DateFormat('dd/MM/yyyy').format(this);
+  String get toTimeHM => DateFormat('HH:mm').format(this);
+  String get formatToUsaString =>
+      DateFormat('yyyy-MM-dd HH:mm:ss').format(this);
+}
 
 extension StringExtensions on String {
   String capitalize() {
