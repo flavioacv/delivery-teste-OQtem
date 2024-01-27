736afa7a9c4f17699d9bc212ba6f7e5d54477cc9
Flavio Augusto <flavioacv.soft@gmail.com>
2024-01-27 15:20:57 -0400

    ajuste

--- lib/app/core/constants/locales/order_locales.dart
+++ lib/app/core/constants/locales/order_locales.dart
@@ -2,6 +2,6 @@ class OrderLocales {
   static const String statusOrder = 'tatus do pedido';
   static const String statusOrderEnd = 'Status do pedido Entregar até';
   static const String orderEnd = 'Entregar até';
-  static const String details = 'Status do pedido Entregar até';
+  static const String details = 'Ver detalhes';
   static const String chat = 'Conversas';
 }
